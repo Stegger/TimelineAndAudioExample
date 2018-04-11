d43c26c621ab0403b73896eac34212a106a87b78
EASV\pgn <stegger.peter@gmail.com>
2018-04-11 14:36:30 +0200

    Initial commit

---
+++ Assets/MyPlayableController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class MyPlayableController : MonoBehaviour {
+
+    private PlayableDirector director;
+
+	// Use this for initialization
+	void Start () {
+        director = GetComponent<PlayableDirector>();
+	}
+	
+    public void startTimelineSequence()
+    {
+        director.Play();
+    }
+
+    public void stopTimelineSequence()
+    {
+        director.Stop();
+    }
+
+    public void pauseTimelineSequence()
+    {
+        director.Pause();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
